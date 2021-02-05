26c8e77e187be6039f39d15cbc718ee8fd878f9b
clD11 <23483715+clD11@users.noreply.github.com>
2021-02-05 13:19:16 +0000

    utopian tree

--- src/hackerrank/utopiantree_test.go
+++ src/hackerrank/utopiantree_test.go
@@ -5,38 +5,34 @@ import "testing"
 func TestReturnHeightForCycle0(t *testing.T) {
 	actual := utopianTree(0)
 	expected := int32(1)
-	if actual != expected {
-		t.Errorf("expected %d actual %d", expected, actual)
-	}
+	assertThat(t, actual, expected)
 }
 
 func TestReturnHeightForCycle1(t *testing.T) {
 	actual := utopianTree(1)
 	expected := int32(2)
-	if actual != expected {
-		t.Errorf("expected %d actual %d", expected, actual)
-	}
+	assertThat(t, actual, expected)
 }
 
 func TestReturnHeightForCycle2(t *testing.T) {
 	actual := utopianTree(2)
 	expected := int32(3)
-	if actual != expected {
-		t.Errorf("expected %d actual %d", expected, actual)
-	}
+	assertThat(t, actual, expected)
 }
 
 func TestReturnHeightForCycle3(t *testing.T) {
 	actual := utopianTree(3)
 	expected := int32(6)
-	if actual != expected {
-		t.Errorf("expected %d actual %d", expected, actual)
-	}
+	assertThat(t, actual, expected)
 }
 
 func TestReturnHeightForCycle5(t *testing.T) {
 	actual := utopianTree(5)
 	expected := int32(14)
+	assertThat(t, actual, expected)
+}
+
+func assertThat(t *testing.T, actual int32, expected int32) {
 	if actual != expected {
 		t.Errorf("expected %d actual %d", expected, actual)
 	}
