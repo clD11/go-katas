d053d8605a56a0c9cb2fe8219c84ab87cb9da0f3
clD11 <23483715+clD11@users.noreply.github.com>
2021-10-21 21:40:27 +0100

    added reverse string test

--- src/random/reversestring_test.go
+++ src/random/reversestring_test.go
@@ -5,9 +5,23 @@ import (
 	"testing"
 )
 
-func TestReverseString(t *testing.T) {
+func TestReverseString_Zero(t *testing.T) {
+	input := "dog"
+	expected := "dog"
+	actual := ReverseString(input)
+	testsupport.AssertThatString(t, actual, expected)
+}
+
+func TestReverseString_One(t *testing.T) {
 	input := "big dog"
 	expected := "dog big"
+	actual := ReverseString(input)
+	testsupport.AssertThatString(t, actual, expected)
+}
+
+func TestReverseString_Two(t *testing.T) {
+	input := "along came polly"
+	expected := "polly came along"
 	actual := ReverseString(input)
 	testsupport.AssertThatString(t, actual, expected)
 }
