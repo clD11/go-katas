232daccec7256b57b1ce927a4e48133797d46ab4
clD11 <23483715+clD11@users.noreply.github.com>
2021-10-23 13:08:11 +0100

    trie fun

--- src/interviews/datastructures/binarytree.go
+++ src/interviews/datastructures/binarytree.go
@@ -2,27 +2,23 @@ package datastructures
 
 import "log"
 
-// Binary Trees
+// Binary Trees - node can have at most two children
 
-// Binary Search Tree
-// The key in each Node is greater than or equal to any key stored in the left sub-tree, and less than or equal to any key stored in the right sub-tree
+// Binary Search Tree - the key in each node is greater than or equal to any key stored in the left sub-tree, and less than or equal to any key stored in the right sub-tree
 //			4
 //		2		5
 //	1		3
 
-// Inorder
-// Postorder
-// Preorder
+// Traversal (stack)
 
-// left -> root -> right
-// output = 1,2,3,4,5
+// Inorder (Left, Root, Right): 1,2,3,4,5
 func InorderTraversal(node *Node) {
 	s := newStack()
 	s.Push(node)
 
 	current := node
 
-	for {
+	for { // controls right
 		if s.Len() == 0 {
 			break
 		}
@@ -36,7 +32,7 @@ func InorderTraversal(node *Node) {
 			current = current.left
 		}
 
-		// back track and go right (if right has a left then loop above goes down left and so on until all nodes are visited)
+		// back track and go right (if right has a left then loop above goes down left again until all nodes are visited)
 		current = s.Pop()
 
 		// testing only isEmpty stop root being printed twice
@@ -49,7 +45,7 @@ func InorderTraversal(node *Node) {
 }
 
 // Left -> Right -> Root
-// output = 1,3,2,5,4
+// Postorder (Left, Right, Root): 1,3,2,5,4
 func PostorderTraversal(node *Node) {
 	s1 := newStack()
 	s2 := newStack()
@@ -82,40 +78,6 @@ func PostorderTraversal(node *Node) {
 
 }
 
-// root -> left -> right
-func PreorderTraversal(node *Node) {
-	for {
-		if node == nil {
-			break
-		}
-
-		if node.left == nil {
-			log.Print(node.value)
-			node = node.right
-		} else {
-			c := node.left
-
-			for {
-				if c.right != nil && c.right != node {
-					c = c.right
-				} else {
-					break
-				}
-			}
-
-			if c.right == node {
-				c.right = nil
-				node = node.right
-			} else {
-				log.Print(node.value)
-				c.right = node
-				node = node.left
-			}
-		}
-
-	}
-}
-
 // ** NOTES **
-// All recursion is simpy using stack and values so can be converted to iteration and is simpler for interviews -
+// All recursion is simply using stack and values so can be converted to iteration and is simpler for interviews -
 // lambda calculus (rec) vs turing machine (iter) -> church-turing thesis
